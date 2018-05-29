8118b9a1b860e5da21ac6fd66759c3448960a4c1
whatsalem <zolekwhatsalem@gmail.com>
2018-05-29 23:19:16 +0700

    ffdfd

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,37 @@
-$(".mainlogo").hover(function() {
-	$("video").fadeToggle("slow");
-	$("video").get(0).volume = 0.2;
-	if ($("video").get(0).paused) {
-		$("video").get(0).play();
+$(".mainlogo1").hover(function() {
+	$(".mainlogo1").toggleClass('hover');
+	$(".mainlogo2").toggleClass('hover');
+	$(".mainlogo3").toggleClass('hover');
+	$(".video1").fadeToggle("slow");
+	$(".video1").get(0).volume = 0.2;
+	if ($(".video1").get(0).paused) {
+		$(".video1").get(0).play();
 	} else {
-		$("video").get(0).pause();
+		$(".video1").get(0).pause();
+	}
+});
+
+$(".mainlogo2").hover(function() {
+	$(".mainlogo2").toggleClass('hover');
+	$(".mainlogo3").toggleClass('hover');
+	$(".mainlogo1").toggleClass('hover');
+	$(".video2").fadeToggle("slow");
+	$(".video2").get(0).volume = 0.1;
+	if ($(".video2").get(0).paused) {
+		$(".video2").get(0).play();
+	} else {
+		$(".video2").get(0).pause();
+	}
+});
+$(".mainlogo3").hover(function() {
+	$(".mainlogo2").toggleClass('hover');
+	$(".mainlogo3").toggleClass('hover');
+	$(".mainlogo1").toggleClass('hover');
+	$(".video3").fadeToggle("slow");
+	$(".video3").get(0).volume = 0.15;
+	if ($(".video3").get(0).paused) {
+		$(".video3").get(0).play();
+	} else {
+		$(".video3").get(0).pause();
 	}
 });
