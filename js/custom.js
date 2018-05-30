0e379b824c6e09fe7edb89e46dcc764100440f9d
whatsalem <zolekwhatsalem@gmail.com>
2018-05-30 18:27:27 +0700

    ffdfd

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,27 @@
+setInterval(function(){ 
+	$(".mainlogo1").effect( "shake", {times:4}, 5000  );
+ }, 3000);
+setInterval(function(){ 
+	$(".mainlogo2").effect( "shake", {times:4}, 1000 );
+ }, 5000);
+setInterval(function(){ 
+	$(".mainlogo3").effect( "shake", {times:4}, 1000  );
+ }, 7000);
 $(".mainlogo1").hover(function() {
-	$(".mainlogo1").toggleClass('hover');
 	$(".mainlogo2").toggleClass('hover');
 	$(".mainlogo3").toggleClass('hover');
+	$(".mainlogo1").toggleClass('hover');
+	$(".mainlogo2").stop();
+	$(".mainlogo3").stop();
+	$(".mainlogo1").stop();
 	$(".video1").fadeToggle("slow");
-	$(".video1").get(0).volume = 0.2;
+	$(".video1").get(0).volume = 0.1;
 	if ($(".video1").get(0).paused) {
 		$(".video1").get(0).play();
 	} else {
 		$(".video1").get(0).pause();
 	}
 });
-
 $(".mainlogo2").hover(function() {
 	$(".mainlogo2").toggleClass('hover');
 	$(".mainlogo3").toggleClass('hover');
